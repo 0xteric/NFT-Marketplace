518f398c98e0608c9db763ccc79deb2c9c4b9069
mermajax <mermajax@gmail.com>
2025-06-04 20:49:41 +0200

    Marketplace fees and accept bids

--- src/Marketplace.sol
+++ src/Marketplace.sol
@@ -22,6 +22,8 @@ contract Marketplace is Ownable, ReentrancyGuard {
     }
 
     uint public marketplaceFee;
+    uint public basicPoints = 10000;
+    address public feeReceiver;
 
     mapping(address => mapping(uint => Listing)) public listings;
     mapping(address => mapping(address => CollectionBid)) public bids;
@@ -34,11 +36,13 @@ contract Marketplace is Ownable, ReentrancyGuard {
 
     constructor(uint _initialFee) Ownable(msg.sender) {
         marketplaceFee = _initialFee;
+        feeReceiver = msg.sender;
     }
 
     function list(address _collection, uint _tokenId, uint _price) external {
         require(_price > 0, "Price must be greater");
         require(IERC721(_collection).ownerOf(_tokenId) == msg.sender, "Not owner");
+        require(IERC721(_collection).isApprovedForAll(msg.sender, address(this)), "Marketplace: collection not approved");
 
         Listing memory newListing = Listing({collection: _collection, seller: msg.sender, tokenId: _tokenId, price: _price});
 
@@ -78,6 +82,32 @@ contract Marketplace is Ownable, ReentrancyGuard {
         emit CancelBid(msg.sender, _collection);
     }
 
+    function acceptBid(address _collection, address _bidder, uint[] calldata _tokensId) external nonReentrant {
+        CollectionBid memory _bid = bids[_collection][_bidder];
+        require(_bid.quantity >= _tokensId.length, "Marketplace: quantity exceeds bid");
+        require(IERC721(_collection).balanceOf(msg.sender) >= _tokensId.length, "Marketplace: not enough balance");
+        require(IERC721(_collection).isApprovedForAll(msg.sender, address(this)), "Marketplace: collection not approved");
+
+        if (_bid.quantity == _tokensId.length) {
+            delete bids[_collection][_bidder];
+        } else {
+            bids[_collection][_bidder].quantity -= _tokensId.length;
+        }
+
+        for (uint i = 0; i < _tokensId.length; i++) {
+            IERC721(_collection).safeTransferFrom(msg.sender, _bid.bidder, _tokensId[i]);
+        }
+
+        uint fee = (_bid.price * _tokensId.length * marketplaceFee) / basicPoints;
+        uint payment = _bid.price * _tokensId.length - fee;
+
+        (bool _success, ) = msg.sender.call{value: payment}("");
+        require(_success, "Accept bid pay transfer failed!");
+
+        (bool __success, ) = feeReceiver.call{value: fee}("");
+        require(__success, "Accept bid fee transfer failed!");
+    }
+
     function buy(address _collection, uint _tokenId) external payable nonReentrant {
         Listing memory _listing = listings[_collection][_tokenId];
         require(_listing.price > 0, "Listing not exists");
@@ -85,10 +115,20 @@ contract Marketplace is Ownable, ReentrancyGuard {
 
         delete listings[_collection][_tokenId];
 
-        (bool success, ) = _listing.seller.call{value: _listing.price}("");
-        require(success, "Transfer failed");
+        uint fee = ((_listing.price * marketplaceFee) / basicPoints);
+        uint payment = _listing.price - fee;
+
+        (bool _success, ) = _listing.seller.call{value: payment}("");
+        require(_success, "Buy pay transfer failed");
+
+        (bool __success, ) = feeReceiver.call{value: fee}("");
+        require(__success, "Buy fee transfer failed");
 
         IERC721(_collection).safeTransferFrom(_listing.seller, msg.sender, _listing.tokenId);
         emit Sale(_listing.seller, msg.sender, _tokenId);
+    }
+
+    function updateMarketplaceFee(uint _newFee) external onlyOwner {
+        marketplaceFee = _newFee;
     }
 }
